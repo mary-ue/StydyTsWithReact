488faadabba13d39b051cc0fc0dba621ac217667
mary-ue <maryandreeva100@gmail.com>
2023-09-04 20:14:35 +0300

    add api and preloader

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Product } from './components/Product';
-import { products } from './data/products';
+import axios from 'axios';
+import { IProduct } from './models';
 
 function App() {
+  const [products, setProducts] = useState<IProduct[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  async function fetchProducts() {
+    setLoading(true);
+    const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5');
+    setProducts(response.data);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchProducts()
+  }, [])
+
   return (
     <div className='container mx-auto max-w-2xl pt-5'>
-      <Product product={products[0]} />
-      <Product product={products[1]} />
+      {loading && <p className='text-center'>Loading...</p>}
+      {
+        products.map(product => <Product key={product.id} product={product} />)
+      }
     </div>
   );
 }
