998712a035dddd58aa6e860ffd6db89f65067d91
mary-ue <maryandreeva100@gmail.com>
2023-09-05 01:11:03 +0300

    add Context

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,16 @@ import { Loader } from './components/Loader';
 import { Modal } from './components/Modal';
 import { Product } from './components/Product';
 import { useProducts } from './hooks/products';
-import { useState } from 'react';
+import { useContext } from 'react';
 import { IProduct } from './models';
+import { ModalContext } from './context/ModalContext';
 
 function App() {
   const { loading, error, products, addProduct } = useProducts();
-  const [modal, setModal] = useState(false);
+  const {modal, open: openModal, close: closeModal} = useContext(ModalContext)
 
   const createHandler = (product: IProduct) => {
-    setModal(false);
+    closeModal();
     addProduct(product);
   };
 
@@ -24,13 +25,13 @@ function App() {
         <Product key={product.id} product={product} />
       ))}
       {modal && (
-        <Modal title="Create new product" onClose={() => setModal(false)}>
+        <Modal title="Create new product" onClose={closeModal}>
           <CreateProduct onCreate={createHandler} />
         </Modal>
       )}
       <button 
         className="fixed bottom-5 right-5 rounded-full bg-red-700 text-white text-2xl px-4 py-2"
-        onClick={() => setModal(true)}
+        onClick={openModal}
       >
         +
       </button>
