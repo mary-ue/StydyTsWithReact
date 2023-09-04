35d40fd158d8af577aaf6f317b90704a32f78007
mary-ue <maryandreeva100@gmail.com>
2023-09-04 19:34:11 +0300

    add Product card

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,7 +1,33 @@
-export function Product() {
+import { useState } from 'react';
+import { IProduct } from "../models"
+
+interface ProductProps {
+  product: IProduct
+}
+
+export function Product({product}: ProductProps) {
+  const [details, setDetails] = useState(false);
+
+  const btnBgClassName = details ? 'bg-yellow-400' : 'bg-blue-400';
+  const btnClasses = ['py-2 px-4 border', btnBgClassName];
+
   return (
-    <>
-      Product!
-    </>
+    <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">
+      <img className="w-1/6" src={product.image} alt={product.title}/>
+      <p>{product.title}</p>
+      <p className="font-bold">{product.price}</p>
+      <button 
+        className={btnClasses.join(' ')}
+        onClick={() => setDetails(prev => !prev)}
+        >
+          { details ? 'Hide Details' : 'Show Details'}
+        </button>
+        { details && 
+          <div>
+            <p>{product.description}</p>
+            <p>Rate: <span style={{ fontWeight: 'bold'}}>{product.rating.rate}</span></p>
+          </div>
+        }
+    </div>
   )
 }
